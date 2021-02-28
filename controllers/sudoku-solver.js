6f0b027ec2cdf01eea81841e1c7d76f63c2ead31
Charles C <charles.yhcheng@gmail.com>
2021-02-28 23:18:56 +0000

    DONE "check placement"

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -3,8 +3,8 @@ class SudokuSolver {
   validate(puzzleString) {
     let rtn = {};
     let regex = /[1-9\.]/
-    if (puzzleString.length!=81){
-      rtn = {"error": "Expected puzzle to be 81 characters long"};
+    if (puzzleString.length != 81) {
+      rtn = { "error": "Expected puzzle to be 81 characters long" };
     } else if (!regex.test(puzzleString)) {
       rtn = { "error": "Invalid characters in puzzle" };
     } else {
@@ -13,82 +13,91 @@ class SudokuSolver {
     return rtn;
   }
 
+  // row & column to be inputted as index (0-8)
   checkRowPlacement(puzzleString, row, column, value) {
-    let regex=/[1-9]/
+    let regex = /[1-9]/
     if (!regex.test(value)) {
       return false;
     } else {
-      let curRow = puzzleString.slice(row*9,row*9+8);
-      console.log(`curRow: ${curRow}`);
-      if (curRow.includes(value)) {
+      let curRow = puzzleString.slice(row * 9, row * 9 + 9);
+      //console.log(`curRow: ${curRow}`);
+      if (curRow.includes(value*1)) {
         return false;
       } else {
         return true;
       }
-    }   
+    }
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-    let regex=/[1-9]/
+    let regex = /[1-9]/
     if (!regex.test(value)) {
       return false;
     } else {
-      for (let i=0;i<9;i++){
-        let puzzleString[row*9+column] = curPlace
-        if (curPlace==value) return false;
+      for (let i = 0; i < 9; i++) {
+        let curPlace = puzzleString[i*9 + column]
+        if (curPlace == value*1) return false;
       }
       return true;
-    } 
+    }
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
     let regex = /[1-9]/
-    const rowReg = (row)=>(Math.floor(row/3));
-    const colReg = (column)=>(Math.floor(column/3));  
+    if (!regex.test(value)) return false;
+
+    const rowReg = (row) => (Math.floor(row / 3));
+    const colReg = (column) => (Math.floor(column / 3));
     let curRowReg = rowReg(row);
     let curColReg = colReg(column);
-    let regArr = puzzleString.filter((x,i)=>{
-      if (rowReg(i)==curRowReg) {
+    //console.log(`curRowReg: ${curRowReg}`)
+    //console.log(`curColReg: ${curColReg}`)
+    let regArr = puzzleString.split("");
+    //console.log(`regArr before: ${regArr}`)
+    regArr = regArr.filter((x, i) => {
+      if (rowReg(Math.floor(i / 9)) == curRowReg && colReg((i % 9)) == curColReg) {
         return true;
       } else {
         return false;
       }
     })
-    
-    let curReg = 0;
-    const curArr = (row,col)=>{
-      puzzleString[]
-    }
-    if (row<3) {
-      if (col<3) {
-        curReg = 0;
-      } else if (col<6) {
-        curReg = 1;
-      } else {
-        curReg = 2;
-      }
-    } else if (row<6) {
-      if (col<3) {
-        curReg = 3;
-      } else if (col<6) {
-        curReg = 4;
-      } else {
-        curReg = 5;
-      }
+    console.log(`regArr: ${regArr}`)
+
+    if (regArr.includes(value*1)) {
+      return false;
     } else {
-      if (col<3) {
-        curReg = 6;
-      } else if (col<6) {
-        curReg = 7;
-      } else {
-        curReg = 8;
-      }
+      return true;
     }
-    
   }
 
   solve(puzzleString) {
-    
+    if (!puzzleString.includes('.')){
+      console.log(`solved!: ${puzzleString}`)
+      return puzzleString;
+    } else {
+      let place = puzzleString.indexOf('.');
+      console.log(`place: ${place}`)
+      for (let i=1;i<=9;i++){
+        let row = Math.floor(place/9);
+        let col = place%9;
+        console.log(`row: ${row}, col: ${col}`)
+        let nextStr = puzzleString.slice(0,place).concat(i,puzzleString.slice(place+1,));
+        let n = i.toString();
+        console.log(n);
+        let checked = this.checkRowPlacement(puzzleString,row,col,n)
+                      && this.checkRowPlacement(puzzleString,row,col,n)
+                      && this.checkRegionPlacement(puzzleString,row,col,n)
+        console.log(`checkRegionPlacement: ${this.checkRegionPlacement(puzzleString,row,col,n)}`)
+        console.log(`checked: ${checked}`)
+        if (checked) {
+          console.log(`nextStr: ${nextStr}`)
+          return this.solve(nextStr);
+        }
+        console.log(i)
+      }
+      
+      return null
+    }
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -2,19 +2,59 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
-      puzzleStr = req.body
-      if (solver.validate(puzzleStr)
+      let puzzleStr = req.body.puzzle;
+      let coor = req.body.coordinate.toLowerCase();
+      let val = req.body.value;
+      console.log(puzzleStr, coor, val);
+
+      let row = coor.charCodeAt(0)-97;
+      let col = coor[1]-1;
+      console.log(`row: ${row}, col: ${col}`)
+      let coorRegex = /^[a-zA-Z][1-9]$/;
+      let valRegex = /^[1-9]{1}$/;
+      if (solver.validate(puzzleStr) != 'validated') {
+        console.log(solver.validate(puzzleStr))
+        res.json(solver.validate(puzzleStr));
+      } else if (!coorRegex.test(coor)) {
+        res.json({ "error": "Invalid coordinate" });
+      } else if (!valRegex.test(val)){
+        res.json({"error": "Invalid value" });
+      } else {
+        let rowCheck = solver.checkRowPlacement(puzzleStr, row, col, val);
+        let colCheck = solver.checkColPlacement(puzzleStr, row, col, val);
+        let regCheck = solver.checkRegionPlacement(puzzleStr, row, col, val);
+        let validity = true;
+        let confArr = [];
+        if (rowCheck == true && colCheck == true && regCheck == true) {
+          validity = true;
+          res.json({ "valid": validity });
+        } else {
+          validity = false;
+          !rowCheck && confArr.push("row");
+          !colCheck && confArr.push("column");
+          !regCheck && confArr.push("region");
+          res.json({ "valid": validity, "conflict": confArr })
+        }
+      }
 
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
-
+      let puzzleStr = req.body.puzzle
+      console.log(`puzzleStr: ${puzzleStr}`)
+      let result = solver.solve(puzzleStr);
+      if (result==null){
+        res.json({ "error": "Puzzle cannot be solved" });
+      } else {
+        console.log(result);
+        res.json(result);
+      }
     });
 };
